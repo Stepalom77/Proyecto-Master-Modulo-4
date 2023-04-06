86f00ce7a00ac4bcea601102301dc76903e66ecf
Stepalom77 <stepalom22@gmail.com>
2023-04-06 14:38:31 -0500

    Agregando search bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
+import SearchBar from './components/SearchBar/SearchBar';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <SearchBar/>
     </div>
   );
 }
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,1 +1,16 @@
 import './SearchBar.css'
+
+const SearchBar = () => {
+    return(
+        <nav className="navbar">
+        <div className="container-fluid d-flex justify-content-center ">
+          <form className="d-flex " role="search">
+            <input className="form-control " type="search" placeholder="Search" aria-label="Search"/>
+            <button className="btn btn-outline-secondary" type="submit">Search</button>
+          </form>
+        </div>
+      </nav>
+    )
+}
+
+export default SearchBar
