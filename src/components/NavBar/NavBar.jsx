3a6cbd65ec8ed61090f7f3512b7279d13d2cd604
Stepalom77 <stepalom22@gmail.com>
2023-04-06 12:34:26 -0500

    Agregando Bootstrap, Awesome Fonts, componente Profile y html de componente NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,1 +1,26 @@
 import './NavBar.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleUser, faBolt } from '@fortawesome/free-solid-svg-icons'
+
+const NavBar = () => {
+    return(
+        <nav className="navbar navbar-expand-lg bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand" href='/'> <FontAwesomeIcon icon={faBolt} /> three pics</a>
+                <button className="navbar-toggler border border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" 
+                 aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                    <FontAwesomeIcon icon={faCircleUser} />
+                </button>
+                <div className="collapse navbar-collapse" id="navbarText">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page"  href='/'>Profile</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
